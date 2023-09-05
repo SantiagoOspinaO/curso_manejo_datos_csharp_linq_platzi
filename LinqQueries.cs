c35fc1f4d6975b3e7cd9e0c52f01f1d3ef542bc4
SantiagoOspinaO <santiago.m200@outlook.es>
2023-09-05 14:20:57 -0500

    Se usa el operador Where

--- LinqQueries.cs
+++ LinqQueries.cs
@@ -1,24 +1,45 @@
+using System.Text.Json;
 using curso_manejo_datos_csharp_linq.Models;
 
 namespace curso_manejo_datos_csharp_linq
 {
-    public class LinqQueries
+  public class LinqQueries
+  {
+    private readonly List<Book> _booksList;
+    private const string FILE_PATH = "books.json";
+
+    public LinqQueries()
     {
-      private List<Book> books = new List<Book>();
-
-        public LinqQueries()
+      _booksList = new List<Book>();
+      if (File.Exists(FILE_PATH))
+      {
+        using StreamReader reader = new(FILE_PATH);
+        string json = reader.ReadToEnd();
+        var options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+        List<Book>? books = JsonSerializer.Deserialize<List<Book>>(json, options);
+        if (books != null)
         {
-          using(StreamReader reader = new StreamReader("books.json"))
-          {
-            string json = reader.ReadToEnd();
-            books = System.Text.Json.JsonSerializer.Deserialize<List<Book>>(json, new System.Text.Json.JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
-                     
-          }
+          this._booksList = books;
         }
-
-        public IEnumerable<Book> GetBooks()
-        {
-          return books;
-        }
+      }
     }
+
+    public IEnumerable<Book> GetBooks() => _booksList;
+
+    public IEnumerable<Book> GetBooksAfter2000()
+    {
+      //return _booksList.Where(book => book.publishedDate.Year > 2000);
+      return from book in _booksList
+             where book.publishedDate.Year > 2000
+             select book;
+    }
+
+    public IEnumerable<Book> GetBooksOver250Pages()
+    {
+      // return _booksList.Where(book => book.pageCount > 250 && book.title.Contains("in Action"));
+      return from book in _booksList
+             where book.pageCount > 250 && book.title.Contains("in Action")
+             select book;
+    }
+  }
 }
--- Program.cs
+++ Program.cs
@@ -3,13 +3,39 @@ using curso_manejo_datos_csharp_linq.Models;
 
 LinqQueries linqQueries = new LinqQueries();
 
-imprimir(linqQueries.GetBooks());
+//imprimir(linqQueries.GetBooks());
+//imprimir(linqQueries.GetBooksAfter2000());
+//imprimir(linqQueries.GetBooksOver250Pages());
 
 void imprimir(IEnumerable<Book> listBooks)
 {
-  Console.WriteLine("{0,-60} {1,15} {2,15}\n", "Title", "Page count", "Published date");
+  Console.WriteLine("\n{0,-60} {1,15} {2,15}\n", "Title", "Page count", "Published date");
   foreach (var book in listBooks)
   {
     Console.WriteLine("{0,-60} {1,15} {2,15}", book.title, book.pageCount, book.publishedDate.ToShortDateString());
   }
 }
+
+do
+{
+  Console.WriteLine("Select an option\n1. Get all books\n2. Get all books after 2000\n3. Get all books over 250 pages and contains in Action\n4. Exit");
+  Console.Write("-> ");
+
+  switch (Console.ReadKey().KeyChar)
+  {
+    case (char)ConsoleKey.D1:
+      imprimir(linqQueries.GetBooks());
+      break;
+    case (char)ConsoleKey.D2:
+      imprimir(linqQueries.GetBooksAfter2000());
+      break;
+    case (char)ConsoleKey.D3:
+      imprimir(linqQueries.GetBooksOver250Pages());
+      break;
+    case (char)ConsoleKey.D4:
+      Console.WriteLine("\nBye!");
+      Environment.Exit(0);
+      break;
+  }
+
+} while (Console.ReadKey().KeyChar != 4);
